4a827b78d260552179123f0479994bd9d0e749c2
gcoter <guillaume.coter@gmail.com>
2017-02-15 10:18:18 +0100

    Methods to split dataset and create batchs

--- seq2seq_chatbot/data/classes/dataset.py
+++ seq2seq_chatbot/data/classes/dataset.py
@@ -5,6 +5,7 @@ X contains sentences and Y contains the corresponding answers (encoded as lists
 of indices, defined thanks to the vocabulary).
 """
 from __future__ import print_function
+import numpy as np
 
 from seq2seq_chatbot.data import io
 from seq2seq_chatbot.data.classes.vocabulary import Vocabulary
@@ -19,14 +20,62 @@ class Dataset(object):
 
 	@staticmethod
 	def load_from_folder(folder_path):
+		print("Loading Dataset from",folder_path)
 		return Dataset.load(voc_path=folder_path + 'voc.json',
 			X_path=folder_path + 'X.npz',
 			Y_path=folder_path + 'Y.npz')
 
+	@staticmethod
+	def get_batchs(X,Y,batch_size,batch_id):
+		assert batch_id in range(len(X)//batch_size)
+		start_index = batch_id * batch_size
+		end_index = (batch_id + 1) * batch_size
+		return X[start_index:end_index], Y[start_index:end_index]
+
+	@staticmethod
+	def get_voc_path(dataset_folder):
+		return dataset_folder + 'voc.json'
+
 	def __init__(self,vocabulary,X_by_buckets,Y_by_buckets):
 		self.vocabulary = vocabulary
+		assert all(bucket_str in Y_by_buckets.keys()
+			for bucket_str in X_by_buckets.keys())
+		assert all(bucket_str in X_by_buckets.keys()
+			for bucket_str in Y_by_buckets.keys())
 		self.X_by_buckets = X_by_buckets
 		self.Y_by_buckets = Y_by_buckets
+		self.buckets = self.X_by_buckets.keys()
+
+	def get_buckets(self):
+		return self.buckets
+
+	def get_data_for_bucket(self,bucket):
+		bucket_str = str(bucket)
+		assert bucket_str in self.X_by_buckets.keys()
+		assert bucket_str in self.Y_by_buckets.keys()
+		X = self.X_by_buckets[bucket_str]
+		Y = self.Y_by_buckets[bucket_str]
+		return X, Y
+
+	def split(self,bucket,test_proportion=0.0):
+		train_X = None
+		train_Y = None
+		test_X = None
+		test_Y = None
+		X, Y = self.get_data_for_bucket(bucket)
+		np.random.shuffle(X)
+		np.random.shuffle(Y)
+		assert len(X) == len(Y)
+		if test_proportion > 0.0:
+			test_index = int(test_proportion * len(X))
+			train_X = X[test_index:]
+			train_Y = Y[test_index:]
+			test_X = X[:test_index]
+			test_Y = Y[:test_index]
+		else:
+			train_X = X
+			train_Y = Y
+		return train_X, train_Y, test_X, test_Y
 
 	def print_data(self):
 		print("*** X and Y by buckets ***")
@@ -49,7 +98,7 @@ class Dataset(object):
 		print("Y saved to",Y_path)
 
 	def save_in_folder(self,folder_path):
-		# create_folder(folder_path) makes sure the folder exists before saving.
+		# create_folder(folder_path) makes sure the folder exists before saving
 		io.create_folder(folder_path)
 		self.save(voc_path=folder_path + 'voc.json',
 			X_path=folder_path + 'X.npz',
