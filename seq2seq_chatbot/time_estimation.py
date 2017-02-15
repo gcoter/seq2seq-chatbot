c20e394347eaecc542146042a68bba5a78ba05de
gcoter <guillaume.coter@gmail.com>
2017-02-15 10:19:37 +0100

    Add TrainingTimeEstimator class

--- seq2seq_chatbot/time_estimation.py
+++ seq2seq_chatbot/time_estimation.py
@@ -26,3 +26,28 @@ class ReadingTimeEstimator(object):
 			remaining_time = self.estimate_time_spent_per_conversation() * num_conversations_left
 			return remaining_time
 		return None
+
+class TrainingTimeEstimator(object):
+	"""
+	"""
+	def __init__(self,num_steps_per_epoch):
+		self.num_steps_per_epoch = num_steps_per_epoch
+		self.previous_times = []
+
+	"""
+	"""
+	def estimate_time_spent_per_step(self):
+		return sum(self.previous_times)/len(self.previous_times)
+
+	"""
+	"""
+	def get_remaining_time(self,
+						   num_epochs_left,
+						   num_steps_left_in_current_epoch,
+						   num_steps_processed_in_time_spent,
+						   time_spent):
+		if num_steps_processed_in_time_spent > 0:
+			self.previous_times.append(float(time_spent)/num_steps_processed_in_time_spent)
+		current_epoch_remaining_time = self.estimate_time_spent_per_step() * num_steps_left_in_current_epoch
+		remaining_time_per_epoch = self.estimate_time_spent_per_step() * self.num_steps_per_epoch
+		return current_epoch_remaining_time + num_epochs_left * remaining_time_per_epoch
